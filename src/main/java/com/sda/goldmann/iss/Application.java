0bdfe33611791cd1b1b553001df4c702fe59de78
unknown <goldmann.daniel.88@gmail.com>
2021-07-24 12:53:26 +0200

    ISS-1: create skeleton app

---
+++ src/main/java/com/sda/goldmann/iss/Application.java
@@ -0,0 +1,13 @@
+package com.sda.goldmann.iss;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Application {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        LOGGER.info("starting iss app");
+    }
+}
